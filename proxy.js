8ab9dce1add9a8dd4c1c5a6cd36ba5acc21842e2
stackkrocket <mohammadadinoyi8@gmail.com>
2024-07-15 16:39:44 +0100

    added some more

--- proxy.js
+++ proxy.js
@@ -25,7 +25,7 @@ app.use((req, res, next) => {
 });
 
 app.get('/', (req, res) => {
-    res.send('you have hit the evms proxy endpoint. Hello there...!!!')
+    res.send('you have hit the evms proxy endpoint. Hello there, welcome...!!!')
 })
 
 app.get('/proxy', async (req, res) => {
